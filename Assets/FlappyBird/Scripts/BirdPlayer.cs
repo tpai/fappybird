3f5211e8053a2a74cfcb8a8b5e9c064091f294c8
tonypai <tony77794@gmail.com>
2015-08-15 00:12:41 +0800

    Project init

---
+++ Assets/FlappyBird/Scripts/BirdPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdPlayer : MonoBehaviour {
+
+	Animator anim;
+	Rigidbody2D rigid;
+
+	public float fwdSpeed = 3.5f;
+	public float flySpeed = 8f;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+//			rigid.AddForce (Vector2.up * 500f);
+			rigid.gravityScale = 1;
+			rigid.velocity = new Vector2 (fwdSpeed, flySpeed);
+		}
+	}
+
+	void FixedUpdate () {
+		anim.SetFloat ("FlySpeed", rigid.velocity.y);
+	}
+}
---
+++ Assets/FlappyBird/Scripts/UI/SystemHint.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SystemHint : MonoBehaviour {
+
+	void Update () {
+		if (Input.anyKeyDown) {
+			Application.LoadLevel (Application.loadedLevelName);
+		}
+	}
+}
